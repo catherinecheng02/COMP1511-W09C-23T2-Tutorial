280d2ae603c3a78e0deb0e356539c4729d82bffa
catherinecheng02 <z5310517@ad.unsw.edu.au>
2023-05-31 11:13:05 +1000

    Create face2.c

---
+++ tut01/face2.c
@@ -0,0 +1,12 @@
+// Written by Catherine Cheng 
+// z5310517 on 31-05-23
+
+#include <stdio.h>
+
+int main(void) {
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf("\\_/\n");
+    return 0;
+}
