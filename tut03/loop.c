a96d1fac3a89ae772cf686ae8abfd0a1ad9f184a
catherinecheng02 <z5310517@ad.unsw.edu.au>
2023-06-14 10:07:48 +1000

    Create loop.c

---
+++ tut03/loop.c
@@ -0,0 +1,12 @@
+// While loop demo
+// Written by Catherine Cheng on 14/16/2023
+#include <stdio.h>
+int main(void) {
+    int i = 0;
+    while (i < 10) {
+        printf("%d\n", i);
+        i = i + 1; // i++
+    }
+
+    return 0;
+}
